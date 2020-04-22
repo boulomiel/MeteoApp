445f65fc918d8749e93369f6afb220c4e58390b6
boulomiel <55102143+boulomiel@users.noreply.github.com>
2020-04-23 00:05:34 +0300

    Update Weather.kt

--- app/src/main/java/com/rubenmimoun/meteoapp/model/Weather.kt
+++ app/src/main/java/com/rubenmimoun/meteoapp/model/Weather.kt
@@ -6,7 +6,6 @@ import java.util.*
 
 data class Weather(
     val name: String,
-   // val description: String,
     val temp: Double,
     val feels_Like: Double,
     val humidity : Double,
@@ -16,18 +15,16 @@ data class Weather(
     companion object {
         @Suppress("UNCHECKED_CAST")
         fun fromAnyDict(dict: AnyDict): Weather? {
-           // val weather = dict["weather"] as? Array<AnyDict>
             val main = dict["main"] as? AnyDict
             val wind = dict["wind"] as? AnyDict
             val name = dict["name"] as? String
-          //  val description = weather?.get(0)?.get("description") as? String
             val temp = main?.get("temp") as? Double
             val feels_like = main?.get("feels_like") as? Double
             var humidity = main?.get("humidity") as? Double
             val speedWind =  wind?.get("speed") as? Double
 
             if (name == null  || temp == null || feels_like == null
-                || humidity == null || speedWind == null //|| description == null
+                || humidity == null || speedWind == null 
             ) {
                 return null
             }
